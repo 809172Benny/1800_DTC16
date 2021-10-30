52a531ff5859264180bad9cacc64d31a1fa45461
Benny Chao <bchao5@my.bcit.ca>
2021-10-30 16:30:24 -0700

    Implemented firebase to login page Redesigned login page

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBWo_PDqpNW79FdKHsGwkYcmoYJMT9QDhA",
+  authDomain: "dtc-16.firebaseapp.com",
+  projectId: "dtc-16",
+  storageBucket: "dtc-16.appspot.com",
+  messagingSenderId: "428198550461",
+  appId: "1:428198550461:web:178a1ec88f00a40cb650d4"
+};
+  
+
+  const app = firebase.initializeApp(firebaseConfig);
+  
