56caa90b888eb2677cb2cdc158b6efb6455978ff
Benny Chao <bchao5@my.bcit.ca>
2021-11-26 01:17:50 -0800

    Implemented user posting functionality

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,58 @@ function getCurrentUserUid() {
     return null;
 }
 
+function writeUserStory() {
+    
+    let Title = document.getElementById("TitleInput").value;
+    let Story = document.getElementById("StoryInput").value;
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid);
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                                                //get user Email
+                    var userEmail = userDoc.data().email;
+                    var userName = userDoc.data().name
+                                                // Start a new collection and add all data in it.
+                    db.collection("Stories").add({
+                        UserID: userID,
+                        UserEmail: userEmail,
+                        UserName: userName,
+                        Title: Title,
+                        Story: Story
+                    });
+                })
+        } else {
+            // No user is signed in.
+                                console.log("no user signed in");
+        }
+    });
+}
+
+function writeStories() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var storiesRef = db.collection("Stories");
+
+    // storiesRef.add({
+    //     code: "BBY01",
+    //     title: "This is my covid story",
+    //     story: "placeholder"
+    // });
+    // storiesRef.add({
+    //     code: "AM01",
+    //     title: "I recently recovered from Covid. AMA",
+    //     story: "placeholder"
+    // });
+    // storiesRef.add({
+    //     code: "NV01",
+    //     title: "This is my covid story 2",
+    //     story: "placeholder"
+    // });
+}
+
 function insertName() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
@@ -25,6 +77,7 @@ function insertName() {
                     //document.getElementById("username").innerText = n;                     //using javascript
                     $("#name-goes-here").text(user_Name); //using jquery
                 })
+                writeStories();
         } else {
             // No user is signed in.
         }
@@ -32,42 +85,20 @@ function insertName() {
 }
 insertName();
 
-function writeStories() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var storiesRef = db.collection("Stories");
-
-    storiesRef.add({
-        code: "BBY01",
-        title: "This is my covid story",
-        story: "fsdnsfh dfsdhfkjsdhfk hhdfkshkdfhsdfhjk"
-    });
-    storiesRef.add({
-        code: "AM01",
-        title: "I recently recovered from Covid. AMA",
-        story: "fsdnsfh dfsdhfkjsdhfk hhdfkshkdfhsdfhjk sdsadsadas"
-    });
-    storiesRef.add({
-        code: "NV01",
-        title: "This is my covid story 2",
-        story: "fsdnsfh dfsdhfkjsdhfk hhdfkshkdfhsdfhjk"
-    });
-}
-//writeStories();
-
 function displayHikes() {
     let hikeCardTemplate = document.getElementById("hikeCardTemplate");
     let hikeCardGroup = document.getElementById("hikeCardGroup");
-    
+
     db.collection("Stories").get()
-        .then(allHikes => {
-            allHikes.forEach(doc => {
-                var hikeName = doc.data().title; //gets the name field
+        .then(snap => {
+            snap.forEach(doc => {
+                var hikeName = doc.data().Title; //gets the name field
                 var hikeID = doc.data().code; //gets the unique ID field
-                var hikeStory = doc.data().story; //gets the name field
-                                        var hikeLength = doc.data().length; //gets the length field
+                var hikeStory = doc.data().Story; //gets the name field
+                var hikeUser = doc.data().UserName; //gets the length field
                 let testHikeCard = hikeCardTemplate.content.cloneNode(true);
                 testHikeCard.querySelector('.card-title').innerHTML = hikeName;
-                testHikeCard.querySelector('.card-length').innerHTML = hikeLength;
+                testHikeCard.querySelector('.card-user').innerHTML = hikeUser;
                 testHikeCard.querySelector('.card-text').innerHTML = hikeStory;
                 testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);
                 testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;
@@ -77,3 +108,54 @@ function displayHikes() {
         })
 }
 displayHikes();
+
+
+// function displayCards(collection) {
+//     let CardTemplate = document.getElementById("CardTemplate");
+
+//     db.collection(collection).get()
+//         .then(snap => {
+//             var i = 1;
+//             snap.forEach(doc => { //iterate thru each doc
+//                 var title = doc.data().title;
+//                 var details = doc.data().story;
+//                 let newcard = CardTemplate.content.cloneNode(true);
+
+//                 //update title and text and image
+//                 newcard.querySelector('.card-title').innerHTML = title;
+//                 newcard.querySelector('.card-text').innerHTML = details;
+//                 newcard.querySelector('.card-image').src = "./images/" + collection + ".jpg";
+
+//                 //give unique ids to all elements for future use
+//                 newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+//                 newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+//                 newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+//                 //attach to gallery
+//                 document.getElementById(collection + "-go-here").appendChild(newcard);
+//                 i++;
+
+//                 newcard.querySelector('.card-href').href = "details.html?collection=" + collection + "?id=" + doc.id; //firestore doc ID
+//             })
+//         })
+// }
+
+// displayCards("Stories");
+// //displayCards("heros");
+// //displayCards("countries);
+// //displayCards("bikes");
+
+// function showDetails() {
+//     // create a URL object
+//     let params = new URL(window.location.href);
+//     let id = params.searchParams.get("id");                   //parse "id"
+//     let collection = params.searchParams.get("collection");   //parse "collection"
+
+//     let message = "Collection is: " + collection;        //build message to display
+//     message += "Document id is:  " + id;    
+//     document.getElementById("details-go-here").innerHTML = message;   
+
+//     // you have collection name, and id; 
+//     // you can now read from db if you want
+// }
+// showDetails();
